280446e77d713de2d6fa071dcbc097c455f2dce2
Simon Owens <so87@evansville.edu>
2016-10-14 10:08:17 -0500

    Fix: Actually added program files

---
+++ Router/Router/kid.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router
+{
+    class kid
+    {
+        private string name;
+        private string address;
+
+        kid(string kid_name, string kid_address)
+        {
+            name = kid_name;
+            address = kid_address;
+        }
+        string getName()
+        {
+            return name;
+        }
+
+        string getAddress()
+        {
+            return address;
+        }
+    }
+}
---
+++ Router/Router/vehicle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Router
+{
+    class vehicle
+    {
+        private int capacity;
+        private string name;
+        private List<kid> kids_list = new List<kid>();
+
+        vehicle(int vehicle_capacity, string vehicle_name)
+        {
+            capacity = vehicle_capacity;
+            name = vehicle_name;
+        }
+
+        int getCapacity()
+        {
+            return capacity;
+        }
+
+        string getName()
+        {
+            return name;
+        }
+        
+
+    }
+}
