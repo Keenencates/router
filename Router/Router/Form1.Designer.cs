37b2a8bd39188d478725b0da28b7a5cd4340c33d
Chris <cj106@evansville.edu>
2016-10-16 16:34:48 -0500

    Added an import button to the kids class that imports a excel file to the kid_list_box.

--- Router/Router/Form1.Designer.cs
+++ Router/Router/Form1.Designer.cs
@@ -36,14 +36,17 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // input_box_kid
             // 
+            this.input_box_kid.AcceptsReturn = true;
             this.input_box_kid.Location = new System.Drawing.Point(12, 65);
             this.input_box_kid.Name = "input_box_kid";
-            this.input_box_kid.Size = new System.Drawing.Size(349, 20);
+            this.input_box_kid.Size = new System.Drawing.Size(304, 20);
             this.input_box_kid.TabIndex = 0;
+            this.input_box_kid.TextChanged += new System.EventHandler(this.input_box_kid_TextChanged);
             // 
             // input_box_vehicle
             // 
@@ -51,6 +54,7 @@
             this.input_box_vehicle.Name = "input_box_vehicle";
             this.input_box_vehicle.Size = new System.Drawing.Size(354, 20);
             this.input_box_vehicle.TabIndex = 1;
+            this.input_box_vehicle.TextChanged += new System.EventHandler(this.input_box_vehicle_TextChanged);
             // 
             // compute_button
             // 
@@ -108,11 +112,22 @@
             this.label3.TabIndex = 7;
             this.label3.Text = "Bus Router";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 20);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "import";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(740, 314);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -138,6 +153,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Router/Router/Form1.cs
+++ Router/Router/Form1.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Router
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
@@ -32,6 +33,64 @@ namespace Router
             kid kid1 = new kid(name, addr);
             return kid1;
         }
+
+        private void input_box_kid_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_box_vehicle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*CJ 10/15/2016
+             Imports a excel spreadsheet with the first box name second box address
+             and saves the excel places as a list of kid objects.
+            */
+            var dialog = new System.Windows.Forms.OpenFileDialog();
+            DialogResult result = dialog.ShowDialog();
+            input_box_kid.Text = dialog.FileName;
+            string fName = dialog.FileName;
+            List<kid> kList = new List<kid>();
+
+            if (fName.Substring(fName.Length - 5) == ".xlsx")
+            {
+                Excel.Application xlApp = new Excel.Application();
+                Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@fName);
+                Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+                Excel.Range xlRange = xlWorksheet.UsedRange;
+
+                int rowCount = xlRange.Rows.Count;
+                int colCount = xlRange.Columns.Count;
+
+                for (int i = 1; i <= rowCount; i++)
+                {
+                    if (xlRange.Cells[i, 1] != null && xlRange.Cells[i, 1].Value2 != null)
+                    {
+                        kList.Add(new kid(xlRange.Cells[i, 1].Value2.ToString(), xlRange.Cells[i, 2].Value2.ToString()));
+                        list_box_kid.Items.Add(xlRange.Cells[i, 1].Value2.ToString() + "\t" + xlRange.Cells[i, 2].Value2.ToString());
+                    }
+                }
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+
+                Marshal.ReleaseComObject(xlRange);
+                Marshal.ReleaseComObject(xlWorksheet);
+
+                xlWorkbook.Close();
+                Marshal.ReleaseComObject(xlWorkbook);
+
+                xlApp.Quit();
+                Marshal.ReleaseComObject(xlApp);
+            }
+
+
+
+        }
+        
 
     }
 }
